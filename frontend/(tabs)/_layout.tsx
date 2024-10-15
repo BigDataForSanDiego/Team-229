7679a1013de9aed9e949858b1867952885e3f682
owenkyang <owentheyanger@gmail.com>
2024-10-14 21:39:31 -0700

    moved folders

--- frontend/(tabs)/_layout.tsx
+++ frontend/(tabs)/_layout.tsx
@@ -1,9 +1,9 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from '@/frontend/components/navigation/TabBarIcon';
+import { Colors } from '@/frontend/constants/Colors';
+import { useColorScheme } from '@/frontend/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
--- frontend/(tabs)/index.tsx
+++ frontend/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/frontend/components/HelloWave';
+import ParallaxScrollView from '@/frontend/components/ParallaxScrollView';
+import { ThemedText } from '@/frontend/components/ThemedText';
+import { ThemedView } from '@/frontend/components/ThemedView';
 
 export default function HomeScreen() {
   return (

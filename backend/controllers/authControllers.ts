d6b71eb6d07e3d5e6aa222747c71efeeb436a5ff
Owen Yang <owentheyanger@gmail.com>
2024-10-15 23:07:40 -0700

    added insurance providers to user schema

--- backend/controllers/authControllers.ts
+++ backend/controllers/authControllers.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import { Response, Request } from "express";
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import { Jwt } from "jsonwebtoken";
+import User from "../models/user";
 
 const secretKey = process.env.JWT_SECRET || 'your-secret-key';
 
@@ -11,5 +13,19 @@ export const login = async (req: Request, res: Response):Promise<void> => {
 }
 
 export const register = async(req: Request, res: Response):Promise<void> => {
+    const {username, password, insurance} = req.body;
 
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          res.status(400).json({ message: 'Username already exists' });
+          return;
+    }
+    const hashed = await bcrypt.hash(password, 10);
+    
+    }
+
+    catch(error){
+
+    }
 }
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,5 +1,7 @@
 import mongoose, { Document, Schema, model } from 'mongoose';
 
+const validInsurances = ['Aetna', 'Blue Cross', 'United Healthcare', 'Cigna', 'Humana'];
+
 export interface IUser extends Document {
   email: string;
   password: string;
@@ -18,7 +20,11 @@ const userSchema = new Schema<IUser>({
     },
     insurance: {
         type: String,
-        required: true
+        required: [true, 'Insurance is required'],
+        enum: {
+            values: validInsurances,
+            message: '{VALUE} not a valid insurance provider'
+        }
     }
 }, {timestamps: true})
 const User = model<IUser>("User", userSchema);

add886a2204458de97e2ff0d7ee1a161379bac0d
Owen Yang <owentheyanger@gmail.com>
2024-10-16 00:29:08 -0700

    implemented jwt request interface

--- backend/middleware/authmiddle.ts
+++ backend/middleware/authmiddle.ts
@@ -1,6 +1,11 @@
-import { Jwt } from "jsonwebtoken";
+import { Jwt, JwtPayload } from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
+
+
+export interface CustomRequest extends Request {
+    user?: JwtPayload;
+}
 
 export const authmiddleware = (req: Request, res: Response, next: NextFunction ) => {
 
